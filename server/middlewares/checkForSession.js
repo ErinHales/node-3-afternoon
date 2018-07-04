1e1e8e040cc1ba6673701c274f5b3252aff5604b
Erin Hales <mcbridee093@gmail.com>
2018-07-04 13:55:52 -0600

    final commit

---
+++ server/middlewares/checkForSession.js
@@ -0,0 +1,8 @@
+module.exports = (req,res,next) => {
+    if(!req.session.user) {
+        req.session.user = {username: '', cart: [], total: 0.00};
+        next();
+    } else {
+        next();
+    }
+}
